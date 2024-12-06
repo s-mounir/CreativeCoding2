5219d6d9ea1a52a6ff300bf97cc10336103d29cb
Sara Mounir <saramounir.pro@gmail.com>
2024-12-06 12:44:55 +0100

    U3 : creating simple rect

--- sketch-skew.js
+++ sketch-skew.js
@@ -1,13 +1,38 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [ 1080, 1080 ]
 };
 
 const sketch = () => {
+  let x, y, w, h;
+
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
+    x = width * 0.5;
+    y = height * 0.5;
+    w = width * 0.6;
+    h = height * 0.1;
+
+    context.save();
+    context.translate(x,y);
+    context.translate(w * -0.5,h * -0.5);
+
+    context.strokeStyle = 'blue';
+    //context.strokeRect(w * -0.5, h * -0.5,w,h);
+
+    context.beginPath();
+    context.moveTo(0, 0);
+    context.lineTo(w, 0);
+    context.lineTo(w, h);
+    context.lineTo(0, h);
+    context.lineTo(0, 0);
+
+    context.stroke();
+
+    context.restore();
   };
 };
 
