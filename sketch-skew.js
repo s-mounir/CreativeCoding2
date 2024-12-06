add424246447c6182f2905e447d6519c8b2af859
Sara Mounir <saramounir.pro@gmail.com>
2024-12-06 16:53:57 +0100

    U3 : multiplying rect

--- sketch-skew.js
+++ sketch-skew.js
@@ -1,39 +1,64 @@
 const canvasSketch = require('canvas-sketch');
+const math = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
+  animate: true
 };
 
-const sketch = () => {
+const sketch = ({ context, width, height }) => {
   let x, y, w, h;
+
+  const num = 20;
+  const degrees = -30;
+
+  const rects = [];
+  for (let i = 0; i < num; i++){
+    x = random.range(0,width);
+    y = random.range(0,height);
+    w = random.range(200,600);
+    h = random.range(40,200);
+
+    rects.push({x,y,w,h});
+  };
 
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    x = width * 0.5;
-    y = height * 0.5;
-    w = width * 0.6;
-    h = height * 0.1;
+    rects.forEach(rect => {
+      const {x,y,w,h} = rect;
+      context.save();
+      context.translate(x,y);
+      context.strokeStyle = 'blue';
 
-    context.save();
-    context.translate(x,y);
-    context.translate(w * -0.5,h * -0.5);
+      drawSkewedRect({context, w, h, degrees});
 
-    context.strokeStyle = 'blue';
-    //context.strokeRect(w * -0.5, h * -0.5,w,h);
-
-    context.beginPath();
-    context.moveTo(0, 0);
-    context.lineTo(w, 0);
-    context.lineTo(w, h);
-    context.lineTo(0, h);
-    context.lineTo(0, 0);
-
-    context.stroke();
-
-    context.restore();
+      context.restore();
+    })
   };
 };
+
+const drawSkewedRect = ({context, w = 600, h = 200, degrees = -45}) => {
+  const angle = math.degToRad(degrees);
+  const rx = Math.cos(angle) * w;
+  const ry = Math.sin(angle) * w;
+
+  
+
+  context.save();
+  context.translate(rx * -0.5,ry * -0.5);
+
+  context.beginPath();
+  context.moveTo(0, 0);
+  context.lineTo(rx, ry);
+  context.lineTo(rx, ry + h);
+  context.lineTo(0, h);
+  context.closePath();
+  context.stroke();
+
+  context.restore();
+}
 
 canvasSketch(sketch, settings);
