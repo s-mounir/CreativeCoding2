7515cac12c4de8ea2caa4b6c23897acc8fc4a925
Sara Mounir <saramounir.pro@gmail.com>
2024-12-07 17:01:21 +0100

    U3 : clipping mask

--- sketch-skew.js
+++ sketch-skew.js
@@ -23,6 +23,13 @@ const sketch = ({ context, width, height }) => {
 
   const bgColors = random.pick(risoColors).hex;
 
+  const mask = {
+    radius: width * 0.4,
+    sides: 3,
+    x: width * 0.5,
+    y: height * 0.5,
+  }
+
   for (let i = 0; i < num; i++){
     x = random.range(0,width);
     y = random.range(0,height);
@@ -41,11 +48,22 @@ const sketch = ({ context, width, height }) => {
     context.fillStyle = bgColors;
     context.fillRect(0, 0, width, height);
 
+
+    context.save();
+    context.translate(mask.x, mask.y);
+
+    drawPolygon({context, radius: mask.radius, sides: mask.sides});
+
+    context.restore();
+    context.clip();
+
+
     rects.forEach(rect => {
       const {x,y,w,h,fill,stroke, blend} = rect;
       let shadowColor;
 
       context.save();
+      //context.translate(-mask.x, -mask.y);
       context.translate(x,y);
       context.strokeStyle = stroke;
       context.fillStyle = fill;
@@ -74,7 +92,21 @@ const sketch = ({ context, width, height }) => {
       context.stroke();
 
       context.restore();
-    })
+    });
+
+    //polygon outline
+    context.save();
+    context.translate(mask.x, mask.y);
+    context.lineWidth = 20;
+
+    drawPolygon({context, radius: mask.radius - context.lineWidth, sides: mask.sides});
+
+    context.globalCompositionOperation = 'color-burn';
+    context.strokeStyle = rectColors[0].hex;
+    context.stroke();
+
+    context.restore();
+    
   };
 };
 
@@ -98,5 +130,18 @@ const drawSkewedRect = ({context, w = 600, h = 200, degrees = -45}) => {
 
   context.restore();
 }
+
+const drawPolygon = ({context, radius = 100, sides = 3 }) => {
+  const slice = Math.PI * 2 / sides;
+  context.beginPath();
+  context.moveTo(0, -radius);
+
+  for (let i = 1; i < sides; i++){
+    const theta = i * slice - Math.PI * 0.5;
+    context.lineTo(Math.cos(theta) * radius, Math.sin(theta) * radius);
+  };
+
+  context.closePath();
+};
 
 canvasSketch(sketch, settings);
