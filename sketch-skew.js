6b3548d68aee1bd4aecaadf9afc58ce660560284
Sara Mounir <saramounir.pro@gmail.com>
2024-12-07 16:12:23 +0100

    U3 : adding random colors

--- sketch-skew.js
+++ sketch-skew.js
@@ -1,6 +1,8 @@
 const canvasSketch = require('canvas-sketch');
 const math = require('canvas-sketch-util/math');
 const random = require('canvas-sketch-util/random');
+const Color = require('canvas-sketch-util/color');
+const risoColors = require('riso-colors');
 
 const settings = {
   dimensions: [ 1080, 1080 ],
@@ -8,32 +10,68 @@ const settings = {
 };
 
 const sketch = ({ context, width, height }) => {
-  let x, y, w, h;
+  let x, y, w, h, fill, stroke, blend;
 
-  const num = 20;
+  const num = 40;
   const degrees = -30;
 
   const rects = [];
+  const rectColors = [
+    random.pick(risoColors),
+    random.pick(risoColors),
+  ];
+
+  const bgColors = random.pick(risoColors).hex;
+
   for (let i = 0; i < num; i++){
     x = random.range(0,width);
     y = random.range(0,height);
-    w = random.range(200,600);
+    w = random.range(600,width);
     h = random.range(40,200);
 
-    rects.push({x,y,w,h});
+    fill = random.pick(rectColors).hex;
+    stroke = random.pick(rectColors).hex;
+
+    blend = (random.value() > 0.5) ? 'overlay' : 'source-over';
+
+    rects.push({x,y,w,h,fill,stroke, blend});
   };
 
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = bgColors;
     context.fillRect(0, 0, width, height);
 
     rects.forEach(rect => {
-      const {x,y,w,h} = rect;
+      const {x,y,w,h,fill,stroke, blend} = rect;
+      let shadowColor;
+
       context.save();
       context.translate(x,y);
-      context.strokeStyle = 'blue';
+      context.strokeStyle = stroke;
+      context.fillStyle = fill;
+      context.lineWidth = 10;
+
+      context.globalCompositionOperation = blend;
 
       drawSkewedRect({context, w, h, degrees});
+
+      shadowColor = Color.offsetHSL(fill, 0, 0, -20);
+      shadowColor.rgba[3] = 0.5;
+
+      context.shadowColor = Color.style(shadowColor.rgba);
+      context.shadowOffsetX = -10;
+      context.shadowOffsetY = 20;
+
+      context.fill();
+
+      context.shadowColor = null;
+      context.stroke();
+      
+      context.globalCompositionOperation = 'source-over';
+
+      context.lineWidth = 2;
+      context.strokeStyle = 'black';
+      context.stroke();
 
       context.restore();
     })
